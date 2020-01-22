8182194ec5f0a54262e32b1235fa4be97cf54ff6
Vladislav-Aleksanov <v.aleksanov@dunice.net>
2020-01-22 16:37:29 +0300

    Added button to delete one task

--- todo.js
+++ todo.js
@@ -1,27 +1,44 @@
 
 function onPageLoaded() {
-    const input = $("input[type='text']");
-    const ul = $("ul.todo-list");
+    let input = document.querySelector("input[type='text']");
+    let ul = document.querySelector("ul.todo-list");
+    let trash = document.querySelectorAll(".todo-trash");
+    let tasks =  Array.from(document.querySelectorAll("ul.todo-list li"));
 
     function createTodo() {
-        const li = document.createElement("li");
-        const checkbox = document.createElement("input");
-        checkbox.type = "checkbox";
-        const textSpan = document.createElement("span");
-        textSpan.classList.add("todo-text");
-        const newTodo = input.value;
-        textSpan.append(newTodo);
+        let li = document.createElement("li");
+        let checkbox = document.createElement("input");
+        let textSpan = document.createElement("span");
+        let trashSpan = document.createElement("span");
 
-        ul.appendChild(li).append(textSpan);
+        checkbox.type = "checkbox";
+        textSpan.classList.add("todo-text");
+        textSpan.append(input.value);
+        trashSpan.classList.add("todo-trash");
+        trashSpan.append("Delete");
+        listenDeleteTodo(trashSpan);
+        li.append(textSpan, trashSpan);
+        tasks.push(li);
+        tasks.forEach((li) => {ul.append(li);})
+
         input.value = "";
     }
 
+    function listenDeleteTodo(element) {
+        element.addEventListener("click", (event) => {
+            tasks.splice(tasks.indexOf(element.parentElement), 1)
+            element.parentElement.remove();
+        });
+    }
+
     input.addEventListener("keypress", (keyPressed) => {
-        const keyEnter = 13;
-        if (keyPressed.which == keyEnter) {
+        let keyEnter = 13;
+        if ((keyPressed.which == keyEnter) && (input.value !== "")) {
             createTodo();
         }
     });
+
+    
 }
 
 document.addEventListener("DOMContentLoaded", onPageLoaded);
