3fcd8220008ff9ae69bdac0ed932898e62f16433
Vladislav-Aleksanov <v.aleksanov@dunice.net>
2020-01-24 16:38:40 +0300

    Added double click task editing

--- todo.js
+++ todo.js
@@ -1,6 +1,5 @@
-
 function onPageLoaded() {
-    let input = document.querySelector("input[type='text']");
+    let input = document.querySelector(".add-todo");
     let ul = document.querySelector("ul.todo-list");
     let tasks =  Array.from(document.querySelectorAll("ul.todo-list li"));
     let tasksStatus = [];
@@ -9,40 +8,84 @@ function onPageLoaded() {
     
     function createTodo() {
         let li = document.createElement("li");
+        let div = document.createElement("div");
         let checkbox = document.createElement("input");
         let textSpan = document.createElement("span");
         let trashSpan = document.createElement("span");
 
+        div.classList.add("view");
         checkbox.type = "checkbox";
         textSpan.classList.add("todo-text");
         textSpan.append(input.value);
         trashSpan.classList.add("todo-trash");
         trashSpan.append("Delete");
+
         listenDeleteTodo(trashSpan);
         listenCheckTodo(checkbox);
-        li.append(checkbox, textSpan, trashSpan);
+        listenEditTodo(textSpan);
+
+        div.append(checkbox, textSpan, trashSpan);
+        $(li).append(div);
         tasks.push(li);
         tasksStatus.push(false);
-        ul.append(li);
+        $(ul).append(li);
 
         input.value = "";
     }
 
     function listenDeleteTodo(element) {
         element.addEventListener("click", () => {
-            let index = tasks.indexOf(element.parentElement);
+            let index = tasks.indexOf(element.parentElement.parentElement);
             tasks.splice(index, 1);
             tasksStatus.splice(index, 1);
             $(ul).empty();
-            tasks.forEach((elem) => {ul.append(elem)});
+            $(ul).append(tasks);
         });
     }
 
     function listenCheckTodo(element) {
         element.addEventListener("click", () => {
             this.checked;
-            let index = tasks.indexOf(element.parentElement);
+            let index = tasks.indexOf(element.parentElement.parentElement);
             tasksStatus[index] = !tasksStatus[index];
+        });
+    }
+
+    function listenEditTodo(element) {
+        element.addEventListener("dblclick", () => {
+            let index = tasks.indexOf(element.parentElement.parentElement);
+            let editTodoInput = document.createElement("input");
+
+            editTodoInput.type = "text";
+            editTodoInput.addEventListener("keypress", (keyPressed) => {
+                let keyEnter = 13;
+                if ((keyPressed.which == keyEnter) && (editTodoInput.value !== "")) {
+                    let li = document.createElement("li");
+                    let div = document.createElement("div");
+                    let checkbox = document.createElement("input");
+                    let textSpan = document.createElement("span");
+                    let trashSpan = document.createElement("span");
+
+                    div.classList.add("view");
+                    checkbox.type = "checkbox";
+                    textSpan.classList.add("todo-text");
+                    textSpan.append(editTodoInput.value);
+                    trashSpan.classList.add("todo-trash");
+                    trashSpan.append("Delete");
+
+                    listenDeleteTodo(trashSpan);
+                    listenCheckTodo(checkbox);
+                    listenEditTodo(textSpan);
+
+                    div.append(checkbox, textSpan, trashSpan);
+                    $(li).append(div);
+                    tasks.splice(index, 1, li);
+                    tasksStatus.splice(index, 1, true);
+                    $(ul).empty();
+                    $(ul).append(tasks);                   
+                }
+            });
+            $(element.parentElement).replaceWith(editTodoInput);
         });
     }
 
@@ -82,9 +125,8 @@ function onPageLoaded() {
             return indexes.indexOf(index) == -1;
         });
         $(ul).empty();
-        tasks.forEach((elem) => {ul.append(elem)});
+        $(ul).append(tasks);;
     });
-
     
 }
 
