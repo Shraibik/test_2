80dd181a2c8cc4301aafdd1d8ab3393fd48efd1b
Shraibik <aleksanov.vlad@gmail.com>
2020-01-27 07:52:38 +0300

    lala

--- todo.js
+++ todo.js
@@ -1,93 +1,115 @@
 function onPageLoaded() {
-    let input = document.querySelector(".add-todo");
-    let ul = document.querySelector("ul.todo-list");
-    let tasks =  Array.from(document.querySelectorAll("ul.todo-list li"));
-    let tasksStatus = [];
-    let checkAllButton = document.querySelector(".check-all-btn");
-    let clearCompletedButton =document.querySelector(".clear-completed-btn"); 
-    
-    function createTodo() {
-        let li = document.createElement("li");
-        let div = document.createElement("div");
-        let checkbox = document.createElement("input");
-        let textSpan = document.createElement("span");
-        let trashSpan = document.createElement("span");
+    // let input = document.querySelector(".add-todo");
+    // let ul = document.querySelector("ul.todo-list");
+    // let tasks =  Array.from(document.querySelectorAll("ul.todo-list li"));
+    // let tasksStatus = [];
+    // let checkAllButton = document.querySelector(".check-all-btn");
 
-        div.classList.add("view");
-        checkbox.type = "checkbox";
-        textSpan.classList.add("todo-text");
-        textSpan.append(input.value);
-        trashSpan.classList.add("todo-trash");
-        trashSpan.append("Delete");
+    
+    let input = $(".add-todo");
+    let ul = $("ul.todo-list");
+    let tasks =  $.makeArray($(".todo-item"));
+    let tasksStatus = $.makeArray();
+    let checkAllButton = $(".check-all-btn");
+
+    console.log(tasks);
+
+    function createTodo() {
+        let li = $("<li>");
+        let div = $("<div>");
+        //let checkbox = $("input");
+        let textSpan = $("<span>");
+        let trashSpan = $("<span>Delete</span>");
+
+        $(li).addClass("todo-item");
+        $(div).addClass("view");
+        //checkbox.type = "checkbox";
+        $(textSpan).addClass("todo-text");
+        $(trashSpan).addClass("todo-trash");
 
         listenDeleteTodo(trashSpan);
-        listenCheckTodo(checkbox);
-        listenEditTodo(textSpan);
+        // listenCheckTodo(checkbox);
+        // listenEditTodo(textSpan);
 
-        div.append(checkbox, textSpan, trashSpan);
+        $(div).append(textSpan, trashSpan);
         $(li).append(div);
-        tasks.push(li);
-        tasksStatus.push(false);
-        $(ul).append(li);
 
-        input.value = "";
+        return li
+    }
+
+    function addTodoToEnd(element) {
+        $(element).find(".todo-text").append($(input).val());
+        $(ul).append(element);
+        tasks.push(element);
+        tasksStatus.push(false);
+        $(input).val("");
+        console.log(tasks);
+        console.log(tasksStatus);
     }
 
     function listenDeleteTodo(element) {
-        element.addEventListener("click", () => {
-            let index = tasks.indexOf(element.parentElement.parentElement);
+        $(element).click(() => {
+            //let index = tasks.indexOf($(element).parent().parent());
+            let removingTodo = $(element).parent().parent()[0];
+            let index;
+            tasks.forEach((item, i) => {
+                if (item[0] === removingTodo) index = i;
+            });
+            console.log(index);
+            console.log($(element).parents(".todo-item"));
             tasks.splice(index, 1);
             tasksStatus.splice(index, 1);
             $(ul).empty();
             $(ul).append(tasks);
+            console.log(tasks);
         });
     }
 
-    function listenCheckTodo(element) {
-        element.addEventListener("click", () => {
-            this.checked;
-            let index = tasks.indexOf(element.parentElement.parentElement);
-            tasksStatus[index] = !tasksStatus[index];
-        });
-    }
+    // function listenCheckTodo(element) {
+    //     element.addEventListener("click", () => {
+    //         this.checked;
+    //         let index = tasks.indexOf(element.parentElement.parentElement);
+    //         tasksStatus[index] = !tasksStatus[index];
+    //     });
+    // }
 
-    function listenEditTodo(element) {
-        element.addEventListener("dblclick", () => {
-            let index = tasks.indexOf(element.parentElement.parentElement);
-            let editTodoInput = document.createElement("input");
+    // function listenEditTodo(element) {
+    //     element.addEventListener("dblclick", () => {
+    //         let index = tasks.indexOf(element.parentElement.parentElement);
+    //         let editTodoInput = document.createElement("input");
 
-            editTodoInput.type = "text";
-            editTodoInput.addEventListener("keypress", (keyPressed) => {
-                let keyEnter = 13;
-                if ((keyPressed.which == keyEnter) && (editTodoInput.value !== "")) {
-                    let li = document.createElement("li");
-                    let div = document.createElement("div");
-                    let checkbox = document.createElement("input");
-                    let textSpan = document.createElement("span");
-                    let trashSpan = document.createElement("span");
+    //         editTodoInput.type = "text";
+    //         editTodoInput.addEventListener("keypress", (keyPressed) => {
+    //             let keyEnter = 13;
+    //             if ((keyPressed.which == keyEnter) && (editTodoInput.value !== "")) {
+    //                 let li = document.createElement("li");
+    //                 let div = document.createElement("div");
+    //                 let checkbox = document.createElement("input");
+    //                 let textSpan = document.createElement("span");
+    //                 let trashSpan = document.createElement("span");
 
-                    div.classList.add("view");
-                    checkbox.type = "checkbox";
-                    textSpan.classList.add("todo-text");
-                    textSpan.append(editTodoInput.value);
-                    trashSpan.classList.add("todo-trash");
-                    trashSpan.append("Delete");
+    //                 div.classList.add("view");
+    //                 checkbox.type = "checkbox";
+    //                 textSpan.classList.add("todo-text");
+    //                 textSpan.append(editTodoInput.value);
+    //                 trashSpan.classList.add("todo-trash");
+    //                 trashSpan.append("Delete");
 
-                    listenDeleteTodo(trashSpan);
-                    listenCheckTodo(checkbox);
-                    listenEditTodo(textSpan);
+    //                 listenDeleteTodo(trashSpan);
+    //                 listenCheckTodo(checkbox);
+    //                 listenEditTodo(textSpan);
 
-                    div.append(checkbox, textSpan, trashSpan);
-                    $(li).append(div);
-                    tasks.splice(index, 1, li);
-                    tasksStatus.splice(index, 1, true);
-                    $(ul).empty();
-                    $(ul).append(tasks);                   
-                }
-            });
-            $(element.parentElement).replaceWith(editTodoInput);
-        });
-    }
+    //                 div.append(checkbox, textSpan, trashSpan);
+    //                 $(li).append(div);
+    //                 tasks.splice(index, 1, li);
+    //                 tasksStatus.splice(index, 1, true);
+    //                 $(ul).empty();
+    //                 $(ul).append(tasks);                   
+    //             }
+    //         });
+    //         $(element.parentElement).replaceWith(editTodoInput);
+    //     });
+    // }
 
     tasks.forEach((elem) => {
         let checkboxStatus = false;
@@ -97,36 +119,36 @@ function onPageLoaded() {
         tasksStatus.push(checkboxStatus);
     });
 
-    input.addEventListener("keypress", (keyPressed) => {
+    $(input).keypress((keyPressed) => {
         let keyEnter = 13;
         if ((keyPressed.which == keyEnter) && (input.value !== "")) {
-            createTodo();
+            addTodoToEnd(createTodo());
         }
     });
 
-    checkAllButton.addEventListener("click", () => {
-        let checkboxes = Array.from(document.querySelectorAll('input[type="checkbox"]'));
-        checkboxes.forEach((elem) => {
-            $(elem).prop('checked', true);;
-        });
-        tasksStatus = tasksStatus.map(() => true);
-    });
+    // checkAllButton.addEventListener("click", () => {
+    //     let checkboxes = Array.from(document.querySelectorAll('input[type="checkbox"]'));
+    //     checkboxes.forEach((elem) => {
+    //         $(elem).prop('checked', true);;
+    //     });
+    //     tasksStatus = tasksStatus.map(() => true);
+    // });
 
-    clearCompletedButton.addEventListener("click", () => {
-        let indexes = [];
+    // clearCompletedButton.addEventListener("click", () => {
+    //     let indexes = [];
 
-        tasksStatus.forEach((elem, index) => {
-            if (elem) {indexes.push(index)}
-        });
-        tasks = tasks.filter((elem, index) => {
-            return indexes.indexOf(index) == -1;
-        });
-        tasksStatus = tasksStatus.filter((elem, index) => {
-            return indexes.indexOf(index) == -1;
-        });
-        $(ul).empty();
-        $(ul).append(tasks);;
-    });
+    //     tasksStatus.forEach((elem, index) => {
+    //         if (elem) {indexes.push(index)}
+    //     });
+    //     tasks = tasks.filter((elem, index) => {
+    //         return indexes.indexOf(index) == -1;
+    //     });
+    //     tasksStatus = tasksStatus.filter((elem, index) => {
+    //         return indexes.indexOf(index) == -1;
+    //     });
+    //     $(ul).empty();
+    //     $(ul).append(tasks);;
+    // });
     
 }
 
